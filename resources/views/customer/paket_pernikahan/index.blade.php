c1f1bb8d3161c0d13b4d152bca64831ca09d18ff
Lukita-12 <lukitayapet@gmail.com>
2025-04-10 17:13:43 +0700

    Step 8: Tombol pesan(@can) di paket pernikahan

--- resources/views/customer/paket_pernikahan/index.blade.php
+++ resources/views/customer/paket_pernikahan/index.blade.php
@@ -2,14 +2,16 @@
 
     <div>
         @foreach ($paketPernikahans as $paketPernikahan)
-            <div class="card">
-                <h2>{{ $paketPernikahan->nama_paket }}</h2>
-                <p>Harga: Rp{{ number_format($paketPernikahan->hargaLunas_paket, 0, ',', '.') }}</p>
-                <p>Status: {{ $paketPernikahan->status_paket }}</p>
-                <a href="{{ route('customer.pesanan.create', ['paket-pernikahan' => $paketPernikahan->id]) }}">
-                    Pesan Sekarang
-                </a>
-            </div>
+            @can ('view', $paketPernikahan)
+                <div class="card">
+                    <h2>{{ $paketPernikahan->nama_paket }}</h2>
+                    <p>Harga: Rp{{ number_format($paketPernikahan->hargaLunas_paket, 0, ',', '.') }}</p>
+                    <p>Status: {{ $paketPernikahan->status_paket }}</p>
+                    <a href="{{ route('customer.pesanan.create', ['paket-pernikahan' => $paketPernikahan->id]) }}">
+                        Pesan Sekarang
+                    </a>
+                </div>
+            @endcan
         @endforeach
     </div>
 
