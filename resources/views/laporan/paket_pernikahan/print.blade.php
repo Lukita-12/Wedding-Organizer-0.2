d42ffbd28ecc977943472449dc79b77f57c0da60
Lukita-12 <lukitayapet@gmail.com>
2025-06-03 20:43:45 +0700

    re. laporan

--- resources/views/laporan/paket_pernikahan/print.blade.php
+++ resources/views/laporan/paket_pernikahan/print.blade.php
@@ -1,48 +1,85 @@
-<x-layout-report>
-    <x-slot:laporan>
-        LAPORAN PAKET PERNIKAHAN
-    </x-slot:laporan>
+<x-layout>
+    <div style="background-color: white; font-family:'Times New Roman', Times, serif; text-align: center; padding: 1rem">
 
-    <table>
-        <thead>
-            <tr>
-                <x-table.td variant="head-report">No.</x-table.td>
-                <x-table.td variant="head-report">Nama Paket</x-table.td>
-                <x-table.td variant="head-report">Eksklusif User</x-table.td>
+        <!-- heading -->
+        <div style="margin-bottom: 1rem;">
+            <div> 
+                <p style="font-weight: bold; font-size: x-large;">Laporan Paket Perniakahan</p>
+                <p style="font-weight: bold; font-size: x-large;">Hatma Wedding Organizer</p>
+                <p style="font-weight: bold; font-size: x-large;">Tahun 2025</p>
+            </div>
 
-                <x-table.td variant="head-report">Venue</x-table.td>
-                <x-table.td variant="head-report">Dekorasi</x-table.td>
-                <x-table.td variant="head-report">Tata Rias</x-table.td>
-                <x-table.td variant="head-report">Catering</x-table.td>
-                <x-table.td variant="head-report">Kue Pernikahan</x-table.td>
-                <x-table.td variant="head-report">Fotografer</x-table.td>
-                <x-table.td variant="head-report">Entertainment</x-table.td>
-                <x-table.td variant="head-report">Staff Acara</x-table.td>
+            <div>
+                <p style="font-size: small;">Jalan Sepakat Rt. 33 No. 12 Kelurahan, Pemurus Dalam, Kec.</p>
+                <p style="font-size: small;">Banjarmasin Sel., Kota Banjarmasin, Kalimantan Selatan</p>
+                <p style="font-size: small;">70236</p>
+            </div>
+        </div>
+        
+        <!-- table -->
+        <div style="margin-bottom: 1rem;">
+            <table>
+                <thead>
+                    <tr>
+                        <td>No.</td>
+                        <td>Nama Paket</td>
+                        <td>User</td>
+    
+                        <td>Venue</td>
+                        <td>Dekorasi</td>
+                        <td>Tata Rias</td>
+                        <td>Catering</td>
+                        <td>Kue Pernikahan</td>
+                        <td>Fotografer</td>
+                        <td>Entertainment</td>
+                        <td>Staff Acara</td>
+    
+                        <td>Harga 01</td>
+                        <td>Harga 02</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($paketPernikahans as $paketPernikahan)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $paketPernikahan->nama_paket }}</td>
+                            <td>{{ $paketPernikahan->user->name ?? '-' }}</td>
+    
+                            <td>{{ $paketPernikahan->venueUsaha         ->requestMitra->nama_usaha ?? '-' }}</td>
+                            <td>{{ $paketPernikahan->dekorasiUsaha      ->requestMitra->nama_usaha ?? '-' }}</td>
+                            <td>{{ $paketPernikahan->tataRiasUsaha      ->requestMitra->nama_usaha ?? '-' }}</td>
+                            <td>{{ $paketPernikahan->cateringUsaha      ->requestMitra->nama_usaha ?? '-' }}</td>
+                            <td>{{ $paketPernikahan->kuePernikahanUsaha ->requestMitra->nama_usaha ?? '-' }}</td>
+                            <td>{{ $paketPernikahan->fotograferUsaha    ->requestMitra->nama_usaha ?? '-' }}</td>
+                            <td>{{ $paketPernikahan->entertainmentUsaha ->requestMitra->nama_usaha ?? '-' }}</td>
+                            <td>{{ $paketPernikahan->staff_acara ?? '-' }}</td>
+    
+                            <td>Rp. {{ number_format($paketPernikahan->hargaDP_paket, 0, ',', '.') }}</td>
+                            <td>Rp. {{ number_format($paketPernikahan->hargaLunas_paket, 0, ',', '.') }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
-                <x-table.td variant="head-report">Harga DP</x-table.td>
-                <x-table.td variant="head-report">Harga Lunas</x-table.td>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($paketPernikahans as $paketPernikahan)
-                <tr>
-                    <x-table.td variant="body-report">{{ $loop->iteration }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->nama_paket }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->user->name ?? '-' }}</x-table.td>
+        <!-- footing -->
+        <div style="display: flex; justify-content: flex-end; padding-right: 4rem;">
+            <div style="text-align: center;">
+                <p style="font-size: small;">Hari, 00 Bulan Tanggal</p>
+                <p style="font-weight: bold;">OWNER HATMA GROUP</p>
+                <p style="font-weight: bold; text-decoration: underline; margin-top: 4rem;">Akbar Hatma</p>
+            </div>
+        </div>
+    </div>
+</x-layout>
 
-                    <x-table.td variant="body-report">{{ $paketPernikahan->venueUsaha         ->requestMitra->nama_usaha ?? '-' }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->dekorasiUsaha      ->requestMitra->nama_usaha ?? '-' }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->tataRiasUsaha      ->requestMitra->nama_usaha ?? '-' }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->cateringUsaha      ->requestMitra->nama_usaha ?? '-' }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->kuePernikahanUsaha ->requestMitra->nama_usaha ?? '-' }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->fotograferUsaha    ->requestMitra->nama_usaha ?? '-' }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->entertainmentUsaha ->requestMitra->nama_usaha ?? '-' }}</x-table.td>
-                    <x-table.td variant="body-report">{{ $paketPernikahan->staff_acara ?? '-' }}</x-table.td>
+<style>
+    table, thead, tbody, tr, td {
+    border: 1px solid;
+    }
 
-                    <x-table.td variant="body-report">Rp. {{ number_format($paketPernikahan->hargaDP_paket, 0, ',', '.') }}</x-table.td>
-                    <x-table.td variant="body-report">Rp. {{ number_format($paketPernikahan->hargaLunas_paket, 0, ',', '.') }}</x-table.td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</x-layout-report>
+    table {
+    width: 100%;
+    border-collapse: collapse;
+    }
+</style>
